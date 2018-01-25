d772427fb7c42be216913b7598001d3671ae83ba
Dani Permana <danipermanaputra97@gmail.com>
2018-01-25 07:30:10 +0700

    first commit

---
+++ app/src/main/java/com/example/kyle/reminder/AlarmSetter.java
@@ -0,0 +1,38 @@
+package com.example.kyle.reminder;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+
+import java.util.Calendar;
+
+public class AlarmSetter extends BroadcastReceiver {
+
+  @Override
+  // once phone reboot complete, set back all alarms
+  public void onReceive(Context context, Intent intent) {
+    ReminderDataHelper database = new ReminderDataHelper(context);
+    Cursor cursor = database.getAllItems();
+    try {
+      while (cursor.moveToNext()) {
+
+        String type = cursor.getString(cursor.getColumnIndex(ReminderDataHelper.DB_COLUMN_TYPE));
+        long time = cursor.getLong(cursor.getColumnIndex(ReminderDataHelper.DB_COLUMN_TIME));
+
+        if (type.equals("alert") && time > Calendar.getInstance().getTimeInMillis()) {
+          Intent service = new Intent(context, AlarmService.class);
+          service.setAction(AlarmService.CREATE);
+          service.putExtra("id",
+                  cursor.getInt(cursor.getColumnIndex(ReminderDataHelper.DB_COLUMN_ID)));
+          context.startService(service);
+        }
+      }
+    } finally {
+      cursor.close();
+    }
+
+  }
+
+}
+
---
+++ app/src/main/java/com/example/kyle/reminder/Tanggal.java
@@ -0,0 +1,13 @@
+package com.example.kyle.reminder;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Tanggal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tanggal);
+    }
+}
